8ad6aa828e16cd57fc6396f691488580c05b228f
Bernardo Quintino <b.quintinoguzman@gmail.com>
2020-05-16 14:28:52 -0500

    Agregando plantillas de Tablas

---
+++ default/app/controllers/tables_controller.php
@@ -0,0 +1,34 @@
+<?php
+
+class TablesController extends AppController
+{
+
+    public function index()
+    {
+
+    }
+
+    public function basic()
+    {
+        //View::template("layouts/default");
+    }
+
+    public function table_datatables()
+    {
+
+    }
+    public function table_responsive_datatables()
+    {
+
+    }
+    public function table_filters_datatables()
+    {
+
+    }
+}
+
+
+
+
+
+ 
