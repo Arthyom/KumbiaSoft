cab9eb44dc52bebc2867c47af373c43b5c07fb83
arthyom <a.gonzalezgavina@ugto.mx>
2020-05-27 17:52:25 -0500

    mejorando las datatables para mostrarlas con los acordiones

--- default/app/models/detallepolizas.php
+++ default/app/models/detallepolizas.php
@@ -2,4 +2,10 @@
     
     class detallepolizas extends ActiveRecord{	
     
+        public function detallesPorIdPoliza($id, $page = 1)
+        {   
+            $sql = "SELECT * FROM detallepolizas WHERE IdPoliza == $id";
+            return $this->paginate_by_sql( 'detallepolizas', $sql, 'per_pages: 10', "page: $page"  );
+            # code...
+        }
     }
